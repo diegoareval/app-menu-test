fce8d085764ea96742f8fd9e3a1ca13553183cd6
Diego <diego2000avelar@gmail.com>
2021-02-03 14:54:41 -0600

    renderizando informacion

--- app/screens/Page2.js
+++ app/screens/Page2.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MainContext } from "../context/DataContext";
 import { fields } from "../form/fields";
 import Form from "../form/Form";
 
 export default function Page2() {
+  const { data } = useContext(MainContext);
+  console.warn(data);
   return (
     <View>
       <Text>Page 2</Text>
       <View style={styles.container}>
-        <Form fields={fields} />
+        <View style={styles.fields}>
+          <Text style={styles.label}>Name: {data.name}</Text>
+        </View>
+        <View style={styles.fields}>
+          <Text style={styles.label}>Lastname: {data.lastname}</Text>
+        </View>
+        <View style={styles.fields}>
+          <Text style={styles.label}>Address: {data.address}</Text>
+        </View>
+        <View style={styles.fields}>
+          <Text style={styles.label}>Phone Number: {data.phone}</Text>
+        </View>
       </View>
     </View>
   );
@@ -19,5 +33,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 10,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    width: 400,
+    backgroundColor: "gray"
   },
+  label: {
+    color: "blue",
+    textAlign: "center"
+  }, fields:{
+    marginTop: 10
+  }
 });
